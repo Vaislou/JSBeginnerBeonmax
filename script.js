6412f305060daa5e41502a7be0c7ce8428b9a4bc
Vaislou <vaislou@gmail.com>
2021-05-16 19:00:37 +0300

    init: create a repo

---
+++ script.js
@@ -0,0 +1,26 @@
+/*jslint node: true */
+'use strict';
+
+var leftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+let myName = 'Alex';
+
+
+let persone = {
+    name : 'John',
+    age : 25,
+    isMarried : false
+};
+
+console.log(persone.name);
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+
+console.log(arr[2]);
+
+// alert('Hello');
+
+// let answer = prompt('Есть ли Вам 18?', 'Да');
+// console.log(answer);
+console.log(typeof(NaN));
