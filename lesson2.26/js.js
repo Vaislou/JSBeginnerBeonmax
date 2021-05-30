c8c2ada885726a75eeb2fa2944f89db158a3b505
Vaislou <vaislou@gmail.com>
2021-05-30 22:34:08 +0300

    fix: complete lesson 2.26

---
+++ lesson2.26/js.js
@@ -0,0 +1,29 @@
+let btn = document.querySelectorAll('button'),
+    wrap = document.querySelector('.wrapper'),
+    link = document.querySelector('a');
+
+// btn[0].onclick = function () {
+//     alert('Вы нажали 1-ю кнопку');
+// };
+
+btn[0].addEventListener('click', function(event) {
+    console.log('Произошло событие: ' + event.type + ' на элементе ' + event.target);
+});
+
+wrap.addEventListener('click', function(event) {
+    console.log('Произошло событие: ' + event.type + ' на элементе ' + event.target);
+});
+
+link.addEventListener('click', function(event) {
+    event.preventDefault();
+    console.log('Произошло событие: ' + event.type + ' на элементе ' + event.target);
+});
+
+btn.forEach(function(item) {
+    item.addEventListener('mouseleave', function() {
+        console.log('Вышли');
+    });
+});
+// btn[0].addEventListener('mouseenter', function () {
+//     alert('Вы навели на 1-ю кнопку');
+// });
