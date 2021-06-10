20d8b003dec7bf24fc692f3cb9861d092d0e24a6
Vaislou <vaislou@gmail.com>
2021-06-10 20:01:16 +0300

    fix: complete task #61 (3.8)

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -40,7 +40,7 @@ window.addEventListener('DOMContentLoaded', function() {
 
     //Timer
 
-    let deadline = '2021-06-09';
+    let deadline = '2021-06-19';
 
     function getTimeRemaining(endtime) {
         let t = Date.parse(endtime) - Date.parse(new Date()),
@@ -96,4 +96,30 @@ window.addEventListener('DOMContentLoaded', function() {
     }
 
     setClock('timer', deadline);
+
+    //Modal
+
+    let more = document.querySelector('.more'),
+        descrBtn = document.querySelectorAll('.description-btn'),
+        overlay = document.querySelector('.overlay'),
+        close = document.querySelector('.popup-close');
+
+    more.addEventListener('click', function(){
+        overlay.style.display = 'block';
+        this.classList.add('more-splash');
+        document.body.style.overflow = 'hidden'; //stop scroll
+    });
+
+    descrBtn.forEach(function callback(i) {
+        i.addEventListener('click', function() {
+            overlay.style.display = 'block';
+            document.body.style.overflow = 'hidden';
+        });
+    });
+
+    close.addEventListener('click', function() {
+        overlay.style.display = 'none';
+        more.classList.remove('more-splash');
+        document.body.style.overflow = ''; //start scroll
+    });
 });
